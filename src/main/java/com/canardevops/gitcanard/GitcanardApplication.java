249129cde00ef25ba61bb9b374226aa677ec1f2a
Sejuapig <sejuapig@gmail.com>
2019-03-11 13:37:23 +0100

    Initialisation du projet

---
+++ src/main/java/com/canardevops/gitcanard/GitcanardApplication.java
@@ -0,0 +1,12 @@
+package com.canardevops.gitcanard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcanardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitcanardApplication.class, args);
+    }
+}
